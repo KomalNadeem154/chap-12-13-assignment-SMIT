5fa372cc22714a941b252ec812eed4545e8cfc1b
Komal Nadeem <koomalkhan111@gmail.com>
2024-12-17 20:18:28 +0500

    Add files via upload

---
+++ assignment chap 12 -13/app.js
@@ -0,0 +1,109 @@
+// QUESTION NO:1
+
+// let char=prompt("enter your character");
+// if(!isNaN(char)){
+//     console.log("the input is a number");
+//     }
+//     else{
+//         let ascii = char.charAt(0);
+//         if(ascii >=65 && ascii <=90){
+//             console.log("The input is an uppercase")
+//         }else if (ascii >= 97 && ascii <= 122) {
+//                     console.log("The input is a lowercase letter.");
+//                 } else {
+//                     console.log("The input is neither a number nor an alphabet letter.");
+//                 }
+//     }
+
+    // QUESTION NO:2
+
+// let num1=parseInt(prompt("Enter the first integer:"));
+// let num2=parseInt(prompt("Enter the second integer integer:"));
+
+// if(num1 > num2){
+//     console.log(`${num1} is larger than ${num2}.1`);
+    
+// }
+// else if(num2 > num1){
+//     console.log(`${num2} is larger than ${num1}`);
+    
+// }
+// else{
+//     console.log("Both integer are equal");
+    
+// }
+
+// QUESTION NO:3
+
+// let num =prompt("enter a number");
+// if(num > 0){
+//     console.log("the number is postive");
+// }
+// else if (num<0){
+//     console.log("the number is negative");  
+// }
+// else{
+//     console.log("the number is zero");
+    
+// }
+
+// QUESTION No:4
+
+// let char = prompt("Enter a single character ")
+// if(char.length===1 && "aeiou".includes(char)){
+//     console.log("true");
+    
+// }
+// else{
+//     console.log("false");
+    
+// }
+
+
+// QUESTION No:5
+
+// let passwword="javascript";
+// let user=prompt("Enter your password");
+// if(!user){
+//     console.log("enter your password");
+    
+// }
+// else if(user===passwword){
+//     console.log("Correct! The password you entered matches the original password.");
+// }
+// else{
+//     console.log("Incorrect password");
+    
+// }
+
+// QUESTION NO:6
+
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+
+// else{
+//     greeting = "Good evening";
+// }
+// console.log(greeting);
+
+// QUESTION NO:7
+
+// let time =parseInt(prompt("Enter time in 24-hours clock format"));
+// if(time >=0 && time < 1200){
+//     console.log("Good Morning!");  
+// }
+// else if(time >= 1200 && time < 1700){
+//     console.log("Good Afternoon!!"); 
+// }
+// else if(time >=1700 && time <2100){
+//     console.log("Good Evening!!!");
+// }
+// else if(time >=2100 && time<=2359){
+//     console.log("Good Night!!!");
+// }
+
+
